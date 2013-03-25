79e209b0dc08b5e4658cdaecb99741d606b00959
Gosuke Miyashita <gosukenator@gmail.com>
2013-03-25 17:42:56 +0900

    hosts に登録するのは eth0 の方の IPアドレスにする

--- manifests/lxc.pp
+++ manifests/lxc.pp
@@ -5,7 +5,7 @@ class lxc-test-box::lxc {
   include lxc-test-box::lxc::config
 
   define setup( $ipaddress_eth0 = '', $ipaddress_eth1 ) {
-    host { "$name.lxc-test-box": ip => $ipaddress_eth1 }
+    host { "$name.lxc-test-box": ip => $ipaddress_eth0 }
     container::create { $name:   }
     devices::create   { $name:  require => Lxc::Container::Create[$name] }
     config::create    { $name:
