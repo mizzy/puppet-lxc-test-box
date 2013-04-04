e5ea3baa9d970c9fca759a770341d2d5923ac000
Gosuke Miyashita <gosukenator@gmail.com>
2013-04-04 17:40:35 +0900

    tiny fixes

--- manifests/rootfs/install.pp
+++ manifests/rootfs/install.pp
@@ -2,18 +2,18 @@ class lxc_test_box::rootfs::install {
   $base_dir = $lxc_test_box::rootfs::base_dir
 
   exec { "install lxc for rootfs":
-    command => "/usr/bin/yum -y --installroot=$basedir install lxc",
+    command => "/usr/bin/yum -y --installroot=$base_dir install lxc",
     timeout => 0,
-    unless  => "/bin/rpm -qa --root=$basedir | grep lxc",
+    unless  => "/bin/rpm -qa --root=$base_dir | grep lxc",
   }
 
   exec { "install puppet for rootfs":
-    command => "/usr/bin/yum -y --installroot=$basedir install puppet",
+    command => "/usr/bin/yum -y --installroot=$base_dir install puppet",
     timeout => 0,
-    unless  => "/bin/rpm -qa --root=$basedir | grep puppet",
+    unless  => "/bin/rpm -qa --root=$base_dir | grep puppet",
   }
 
-  exec { "yum install rubygmes for rootfs":
+  exec { "install rubygmes for rootfs":
     command => "/usr/bin/yum -y --installroot=$base_dir install rubygems",
     unless  => "/bin/rpm -qa --root=$base_dir | grep rubygems",
     require => Exec["yum groupinstall core for rootfs"],
