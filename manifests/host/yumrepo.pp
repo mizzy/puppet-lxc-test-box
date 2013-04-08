1c48325ba020cd0f661fe818e79f6f20df739372
Kentaro Kuribayashi <kentarok@gmail.com>
2013-04-08 19:28:24 +0900

    `descr` seems to be required

--- manifests/host/yumrepo.pp
+++ manifests/host/yumrepo.pp
@@ -1,6 +1,6 @@
 class lxc_test_box::host::yumrepo {
   yumrepo { 'rpmforge':
-    name       => 'rpmforge',
+    descr      => 'rpmforge',
     baseurl    => 'http://apt.sw.be/redhat/el6/en/$basearch/rpmforge',
     mirrorlist => 'http://mirrorlist.repoforge.org/el6/mirrors-rpmforge',
     enabled    => 1,
